53335d39e4bfca550a7646e3af3cc823dc5d0036
Thiago Diegoli <thiago_diegoli@hotmail.com>
2024-10-11 21:20:57 -0300

    Removing delete route

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -21,8 +21,5 @@ module.exports = app => {
   // Delete a Tutorial with id
   router.delete("/:id", tutorials.delete);
 
-  // Delete all Tutorials
-  router.delete("/", tutorials.deleteAll);
-
   app.use('/api/tutorials', router);
 };
